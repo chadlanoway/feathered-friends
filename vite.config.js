3d4b22fd83e74eebfd278b1e2c261cee673ab52b
chadlanoway <112406839+chadlanoway@users.noreply.github.com>
2026-09-13 08:40:54 -0500

    publishing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/feathered-friends/",
+    base: "/",
 
     build: {
         rollupOptions: {
